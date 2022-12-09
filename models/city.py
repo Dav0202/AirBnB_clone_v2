58c4375ef78f040a2d90c7a3c57e5b6ad43443d1
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-12-09 01:25:36 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
