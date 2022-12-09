48a7d9ecd92ae8172d0c01671d0a70f227a460f5
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-12-09 01:26:35 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -4,8 +4,8 @@ from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
+class Review(BaseModel, Base):
+    """ Review class to store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
