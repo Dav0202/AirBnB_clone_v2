1f9fcaf5ba21cc2298228c24c798cb9a8297c30f
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-12-09 01:27:15 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -21,7 +21,6 @@ class State(BaseModel, Base):
                               backref="state",
                               cascade="all, delete, delete-orphan")
     else:
-
         @property
         def cities(self):
             """ Returns the list of City instances with """
