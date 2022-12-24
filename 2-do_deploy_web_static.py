8782c92409a7279ade3266691c7c10944c2b767e
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-12-24 23:16:19 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-from fabric.api import put, run, local, env
+""" Does deployment"""
+
+from fabric.api import *
 import os
-import time
 
-env.hosts = ['35.227.25.29', '35.237.11.121']
-
-
-def do_pack():
-    timestr = time.strftime("%Y%m%d%H%M%S")
-    if os.path.exists('./versions') is False:
-        local("mkdir versions")
-    execute = "tar -cvzf ./versions/web_static_{}.tgz web_static".format(
-        timestr)
-    result = local(execute)
-    if result.succeeded:
-        return "versions/web_static_{}".format(timestr)
-    else:
-        return None
+env.hosts = ["35.174.204.230", "54.144.135.186"]
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    if (os.path.exists(archive_path) is False):
-        return None
-
-    name_ext = archive_path.split('/')[-1]
-    name = name_ext.split('.')[0]
-    put(archive_path, '/tmp')
-    location = '/data/web_static/releases/'
-    run('mkdir -p {}{}'.format(location, name))
-    run('tar -xzf /tmp/{} -C {}{}/'.format(name_ext, location, name))
-    run('rm /tmp/{}'.format(name_ext))
-    run('mv {}{}/web_static/* {}{}/'.format(location, name, location, name))
-    run('rm -rf {}{}/web_static'.format(location, name))
-    run('rm -rf /data/web_static/current')
-    res = run('ln -sf {}{}/ /data/web_static/current'.format(location, name))
-    if res.succeeded:
-        print("New version deployed!")
-        return True
-    else:
+    """ Deploys archive to servers"""
+    if not os.path.exists(archive_path):
         return False
+
+    results = []
+
+    res = put(archive_path, "/tmp")
+    results.append(res.succeeded)
+
+    basename = os.path.basename(archive_path)
+    if basename[-4:] == ".tgz":
+        name = basename[:-4]
+    newdir = "/data/web_static/releases/" + name
+    run("mkdir -p " + newdir)
+    run("tar -xzf /tmp/" + basename + " -C " + newdir)
+
+    run("rm /tmp/" + basename)
+    run("mv " + newdir + "/web_static/* " + newdir)
+    run("rm -rf " + newdir + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + newdir + " /data/web_static/current")
+
+    return True
